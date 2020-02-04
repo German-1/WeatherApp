da44265edeb348a25a6598cfd4ee5bda65a61683
german.garcia <german.garcia@tars.dev>
2020-02-04 11:19:57 -0500

    Componentes WeatherInfo y WeatherTemperature

---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+    <div>
+        <span>
+            12 Cº
+        </span>
+    </div>
+);
+
+export default WeatherTemperature;
