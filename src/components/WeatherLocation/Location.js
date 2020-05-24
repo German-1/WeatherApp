c00459496027694b4317b1dce73a7358202d29b8
German <gmgarcia@ucundinamarca.edu.co>
2020-05-24 13:10:53 -0500

    External Font (estilos componente Location)

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = ({ city }) => (    // Destructuring
-    <div>
+    <div className='LocationCont' >
         <h1>
             {city}
         </h1>
