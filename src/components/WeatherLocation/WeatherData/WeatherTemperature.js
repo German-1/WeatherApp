c64f5ca98a086312542886abbca8ce81a054cde3
German <gmgarcia@ucundinamarca.edu.co>
2020-05-24 12:44:25 -0500

    Estilos componente WeatherTemperature

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -31,13 +31,14 @@ const stateToIconName = weatherState => {
 };
 
 const getWeatherIcon = weatherState => {
-    return (<WeatherIcons name={stateToIconName(weatherState)} size="2x" />);
+    return (<WeatherIcons className='wicon' name={stateToIconName(weatherState)} size="4x" />);
 };
 
 const WeatherTemperature = ({ temperature, weatherState }) => (
     <div className='weatherTemperatureCont'>
         {getWeatherIcon(weatherState)}
-        <span>{`${temperature} Cº`}</span>
+        <span className='temperature' >{`${temperature}`}</span>
+        <span className='temperaturetype'>Cº</span>
     </div>
 );
 
