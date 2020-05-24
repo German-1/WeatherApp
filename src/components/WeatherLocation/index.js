871401baff7f4416b52955adb014fafdc3a68acc
German <gmgarcia@ucundinamarca.edu.co>
2020-05-24 14:17:30 -0500

    Evento Onclick y manejo de State

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import { SUN } from './../../constants/weathers';
+import { SUN, WINDY } from './../../constants/weathers';
 import './styles.css';
 
-const data = {
+const data1 = {
     temperature: 20,
     weatherState: SUN,
     humidity: 10,
     wind: '10 m/s',
 };
 
-const WeatherLocation = () => (
-    <div className='weatherLocationCont' >
-        <Location city={'Buenos Aires'} />
-        <WeatherData data={data} />
-    </div>
-);
+const data2 = {
+    temperature: 18,
+    weatherState: WINDY,
+    humidity: 5,
+    wind: '19 m/s',
+};
+
+class WeatherLocation extends Component {
+
+    state = {
+        city: 'Buenos Aires',
+        data: data1,
+    };
+
+    handleUpdateClick = () => {
+        this.setState({
+            city: 'Buenos Aires',
+            data: data2,
+        });
+        console.log("actualizado");
+    }
+
+    render = () => {
+        const { city, data } = this.state;
+        return (
+            <div className='weatherLocationCont' >
+                <Location city={city} />
+                <WeatherData data={data} />
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    }
+}
 
 export default WeatherLocation;
