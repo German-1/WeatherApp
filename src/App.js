c4e88430de4141ec1ae746f65681809adb5598cc
German <gmgarcia@ucundinamarca.edu.co>
2020-05-28 12:51:47 -0500

    Refactorizaciín de LocationList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+  'Buenos Aires,ar',
+  'Bogotá,col',
+  'London,uk',
+  'Washington dc,us',
+  'Ciudad de México,mx',
+  'Madrid,es',
+];
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <LocationList></LocationList>
+          <LocationList cities={cities}></LocationList>
         </div>
       </MuiThemeProvider>
     );
