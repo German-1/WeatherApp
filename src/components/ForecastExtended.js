fc364821e48e96e79c94d64352c2ac86be3ebad5
German <gmgarcia@ucundinamarca.edu.co>
2020-06-11 12:41:17 -0500

    Ejecución de Fetch Forecast

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -30,9 +30,17 @@ class ForecastExtended extends Component {
     }
 
     componentDidMount() {
-        
+        // fetch or axios
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+            }
+        );
     }
-    
+
 
     renderForecastItemDays() {
         return 'Render Items';
