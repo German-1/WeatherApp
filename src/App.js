73a5a6f315829f2a771ecbcc23229805fb11dd17
German <gmgarcia@ucundinamarca.edu.co>
2020-06-16 17:33:17 -0500

    Creación de container LocationListContainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from './components/ForecastExtended';
-import { setCity } from './actions';
 
 import './App.css';
 
@@ -28,13 +25,6 @@ class App extends Component {
     }
   }
 
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    console.log(`handleSelectedLocation ${city}`);
-
-    this.props.setCity(city);
-  }
-
   render() {
     const { city } = this.state;
     return (
@@ -47,11 +37,10 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-              <LocationList
+              <LocationListContainer
                 cities={cities}
-                onSelectedLocation={this.handleSelectedLocation}
               >
-              </LocationList>
+              </LocationListContainer>
             </Col>
             <Col xs={12} md={6}>
               <Paper zDepth={4}>
@@ -70,12 +59,4 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  setCity: PropTypes.func.isRequired,
-}
-
-const mapDispatchToProps = dispatch => ({
-  setCity: value => dispatch(setCity(value))
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
