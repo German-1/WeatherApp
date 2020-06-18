8769fc16f9de7a1d3df8e7a25d97bbef692b9704
German <gmgarcia@ucundinamarca.edu.co>
2020-06-18 12:28:03 -0500

    Refactor action setCity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,34 @@
+import transformForecast from './../services/transformForecast';
+
 export const SET_CITY = 'SET_CITY';
 
-export const setCity = payload => ({ type: SET_CITY, payload });
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
+
+const setCity = payload => ({ type: SET_CITY, payload });
+
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload });
+
+const api_key = "fae4953631559c2eaaa68bcb1b7ce531";
+
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
+export const setSelectedCity = payload => {
+    return dispatch => {
+        const url_forecast = `${url}?q=${payload}&appid=${api_key}`;
+
+        // Activiar en el estado un indicador de busqueda de datos
+        dispatch(setCity(payload));
+
+        return fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+
+                // Modificar el estado con el resultado de la promise (fetch)
+                dispatch(setForecastData({ city: payload, forecastData }));
+            }
+        );
+    };
+};
